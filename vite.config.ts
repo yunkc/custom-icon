7627648a0e00fecbfece4fcf0791b3a3f9f93659
8088 <flasher.jun@gmail.com>
2022-03-23 21:32:52 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import svgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: './',
     plugins: [vue(), svgLoader({ svgo: false })],
     resolve: {
         alias: {
